ddee2ae8a08bb7baa4ae9ded3a36a30a1cbab1c6
Ghada <g.f.alaskar@gmail.com>
2021-03-22 17:51:27 +0300

    Updates

---
+++ precache-manifest.08d26aae56a8a8db3ecdaa069da23a43.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "617f4cb06682e0ac49290e5e59baeda8",
+    "url": "/ghadas/index.html"
+  },
+  {
+    "revision": "99943e65dea46a12a605",
+    "url": "/ghadas/static/js/2.2efd495f.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/ghadas/static/js/2.2efd495f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3abbfc21b13910105eea",
+    "url": "/ghadas/static/js/main.fc0f140a.chunk.js"
+  },
+  {
+    "revision": "4e075b9f9db85346c093",
+    "url": "/ghadas/static/js/runtime-main.d5734be5.js"
+  }
+]);
